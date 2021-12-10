0c8510f29438d7836c8659de59f8e47bb20dd013
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2021-12-10 12:48:45 +0100

    integrated new API + new function to display 5 days forecast
    new API needs coordinates, we get them from initial API. Created getForecast function

--- src/app.js
+++ src/app.js
@@ -21,6 +21,13 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+
+function getForecast(coordinates) {
+  let apiKey = "046cd9bd475a137d5bb80b4c81376f95";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -42,6 +49,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+ getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -73,7 +82,8 @@ function displayCelsius(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
   let forecastHTML = `<div class = "row">`; 
@@ -109,4 +119,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsius);
 
 search("Marseille");
-displayForecast() 

2865702eb4ffbea4f3ab29c125d4994db6fcbab9
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2021-12-13 19:14:32 +0100

    modified temperatures html

--- src/app.js
+++ src/app.js
@@ -100,7 +100,7 @@ function displayForecast(response) {
     <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
       <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" width="60"/>
       <div class="forecast-temperature">
-        <span class="forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span>|<span class="forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
+        <span class="forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span> <span class="forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
       </div>              
   </div>            
 `;}
